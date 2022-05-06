3037dfcf58b88424c0f21879e061090dd5ca3e2a
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2022-05-06 20:44:33 -0300

    💄 basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 type Inputs = {
+  name: string;
   email: string;
 };
+
+const schema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+  })
+  .required();
 
 const App: React.FC = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
   const onSubmit: SubmitHandler<Inputs> = (data) => alert(JSON.stringify(data));
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      Nome
+      <input {...register("name", { required: true })} />
+      <p>{errors.name?.message}</p>
+      Email
       <input {...register("email", { required: true })} />
-      <input type="submit" />
+      <p>{errors.email?.message}</p>
+      <button type="submit">Enviar</button>
     </form>
   );
 };
